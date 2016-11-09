66c9438fdd368a3d11a2890a4fe115b1f3b8d3de
Michael Chen W <michael.cw02@hotmail.com>
2016-11-09 16:54:44 -0600

    Changes on nodo

--- PageRank/PageRank/Nodo.cpp
+++ PageRank/PageRank/Nodo.cpp
@@ -2,17 +2,20 @@
 
 Nodo::Nodo(){
 	nombre = " ";
-	vecinos = list<Nodo*>();
+	outbound = list<Nodo*>();
+	vecinosEntrantes = Map<string, <int>, less<string>>();
+}
+Nodo::Nodo(string nombre) {
+	this.nombre = nombre;
+	outbound = list<Nodo*>();
+	vecinosEntrantes = Map<string, <int>, less<string>>();
 }
 
-
+void Nodo::setNombre(string nombre) {
+	this.nombre = nombre;
+}
 string Nodo::getNombre(){
 	return nombre;
-}
-
-
-void Nodo::agregar(Nodo * n){
-	
 }
 
 string Nodo::toString(){
@@ -20,6 +23,43 @@ string Nodo::toString(){
 	s<<"https//:"<<nombre<<endl;
 	return s.str();
 }
+
+//ITERATORS...
+iterator Nodo::getOutIterator() {
+	return outbound.begin();
+}
+iterator Nodo::getInIterator() {
+	return Map.begin();
+}
+
+//VECINOS SALIENTES...
+void Nodo::agregarNodo(Nodo * n){
+	outbound.push(n);
+}
+bool Nodo::enviarClick(string nom) {
+	outIterator = outbound.begin();
+	while(outIterator != outbound.end()) {
+		string nombre = outIterator.getNombre();
+		if(nombre === nom) {
+			outIterator.recibirClick(nom);
+			return true;	
+		}
+	}
+	return false;
+}
+
+//VECINOS ENTRANTES...
+void Nodo::agregarEntranda(string nom) {
+	//inbound.insert(creaMap::value_type(17, 10.0));
+}
+void Nodo::recibirClick(string nom) {
+	inIterator = inbound.find(nom);
+	if(inIterator != inbound.end()) {
+		inIterator->second++;
+	}
+}
+
+
 
 Nodo::~Nodo(void){
 	/////Investigar sobre el destructor de un map
--- PageRank/PageRank/Nodo.h
+++ PageRank/PageRank/Nodo.h
@@ -10,14 +10,35 @@ using namespace std;
 class Nodo{
 private:
 	string nombre;
-	list<Nodo*> vecinos;
-	typedef multimap < string, int, less<string> > creaMap;
-	creaMap vecinos;
+	list<Nodo*> outbound;
+	typedef multimap < string, int, less<string> > Map;
+	Map inbound;
+	list<Nodo*>::iterator outIterator;
+	Map iterator inIterator;
+
 public:
 	Nodo(void);
+	Nodo(string);
+
+	void setNombre(string);
 	string getNombre();
+	
 	string toString();
-	void agregar(Nodo *);
+
+	//ITERATORS...
+	iterator getOutIterator();
+	iterator getInIterator();
+	
+	//VECINOS SALIENTES....
+	void agregarNodo(Nodo*);
+	bool enviarClick(string); //string = nombre del nodo...
+
+	//VECINOS ENTRANTES....
+	void agregarEntranda(string nom);
+	void recibirClick(string);
+	int getCantVecinos();
+	int getCantClicks();
+
 	~Nodo(void);
 };
 
