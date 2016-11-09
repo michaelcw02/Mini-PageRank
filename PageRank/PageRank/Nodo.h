85b493cb877a1f3af09bb5867745710432241c4d
Michael Chen W <michael.cw02@hotmail.com>
2016-11-09 09:08:24 -0600

    LALALA

--- PageRank/PageRank/Nodo.h
+++ PageRank/PageRank/Nodo.h
@@ -11,11 +11,11 @@ class Nodo{
 private:
 	string nombre;
 	list<Nodo*> vecinos;
-	typedef multimap<string,int, less<string>>creaMap;
+	typedef multimap < string, int, less<string> > creaMap;
 	creaMap vecinos;
 public:
 	Nodo(void);
-	string getNombre();;
+	string getNombre();
 	string toString();
 	void agregar(Nodo *);
 	~Nodo(void);
